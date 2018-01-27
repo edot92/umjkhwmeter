ba68f8b9821a5d5bdcae5c1e3edc3492ce2a3092
Edy Prasetyo <edot92@gmail.com>
2018-01-27 08:28:33 +0700

    add build

--- raspberry/pull.sh
+++ raspberry/pull.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+sleep 5
 while true
 do
 if ping -q -c 1 -W 1 google.com >/dev/null; then
   echo "The network is up"
- gut pull
+ git pull
+sleep 60
+
 else
   echo "The network is down"
 fi
-sleep 10
 done
 
